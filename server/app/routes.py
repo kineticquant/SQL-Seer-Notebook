61d8af4c9b64ddbfbf8f8ccbd9731dc68c231ed7
Moondog <michaelcmooney@outlook.com>
2024-10-24 15:12:59 -0400

    added header bar

--- server/app/routes.py
+++ server/app/routes.py
@@ -9,3 +9,7 @@ templates = Jinja2Templates(directory="client")
 @router.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+#Post > Saving credentials
+# Add to file
