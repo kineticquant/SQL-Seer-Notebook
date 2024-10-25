94113ce0d02a281bc54750623201ff96bff02ad1
Moondog <michaelcmooney@outlook.com>
2024-10-25 14:00:22 -0400

    added connection to sqlite at startup

--- server/app/main.py
+++ server/app/main.py
@@ -1,11 +1,12 @@
 import os
 import logging
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from .middleware import file_config
 from .routes import router
+from . import database, models
 
 app = FastAPI()
 logging.basicConfig(level=logging.INFO)
@@ -13,6 +14,19 @@ logger = logging.getLogger(__name__)
 
 app.include_router(router)
 
+
+# Create the database tables
+database.Base.metadata.create_all(bind=database.engine)
+
+# db session
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+        logger.info("Connected to database successfully.")
+    finally:
+        db.close()
+        
 
 app.mount("/static", StaticFiles(directory="client/static"), name="static")
 templates = Jinja2Templates(directory="client")
